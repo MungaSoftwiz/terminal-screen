d83c8cef3e8ad7f90aadd2ba612e67ad48eb73a9
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-12-01 14:54:03 +0000

    feat: main entry of the program

--- terminal_screen/bin/main.ml
+++ terminal_screen/bin/main.ml
@@ -1,1 +1,16 @@
-let () = print_endline "Hello, World!"
+open Terminal_screen.Screen
+
+let () =
+  let example_setup_data = [| 80; 25; 0x02 |] in
+  try
+    setup_screen example_setup_data;
+    if is_ready () then
+      let state = get_screen_state () in
+      Printf.printf "Screen is ready with dimensions %dx%d and colour mode %s\n"
+        state.width state.height
+        (match state.colour_mode with
+        | Monochrome -> "Monochrome"
+        | SixteenColours -> "16 Colours"
+        | TwoHundredAndFiftySixColours -> "256 Colours")
+    else Printf.printf "Screen is not initialized.\n"
+  with Failure msg -> Printf.printf "Error: %s\n" msg
