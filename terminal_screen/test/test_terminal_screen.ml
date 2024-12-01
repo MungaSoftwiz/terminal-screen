5de21ea29e17bb74186e2183e4e4ca1676c1ffc1
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-12-01 15:02:35 +0000

    test: test module for terminal_screen

---
+++ terminal_screen/test/test_terminal_screen.ml
@@ -0,0 +1,14 @@
+open Terminal_screen.Screen
+
+let test_setup_screen () =
+  let test_data = [| 100; 30; 0x01 |] in
+  setup_screen test_data;
+  assert (is_ready ());
+  let state = get_screen_state () in
+  assert (state.width = 100);
+  assert (state.height = 30);
+  match state.colour_mode with SixteenColours -> () | _ -> assert false
+
+let () =
+  test_setup_screen ();
+  Printf.printf "All tests passed!\n"
