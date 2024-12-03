f059e9f109e76e969f84d25de2e9f16c7098a026
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-12-03 15:59:50 +0000

    test: draw line module test

--- test/test_terminal_screen.ml
+++ test/test_terminal_screen.ml
@@ -1,5 +1,6 @@
 open Terminal_screen.Screen
 open Terminal_screen.Draw_character
+open Terminal_screen.Draw_line
 
 let test_setup_screen () =
   let test_data = [| 100; 30; 0x01 |] in
@@ -18,10 +19,21 @@ let test_draw_character () =
   let screen_buffer = get_screen_buffer () in
   assert (screen_buffer.(5).(10) = 'A')
 
+let test_draw_line () =
+  let test_data = [| 100; 30; 0x01 |] in
+  setup_screen test_data;
+
+  draw_line [| 10; 5; 20; 15; 0x01; Char.code '-' |];
+
+  let screen_buffer = get_screen_buffer () in
+  assert (screen_buffer.(5).(10) = '-');
+  assert (screen_buffer.(15).(20) = '-')
+
 let test_cases =
   [
     ("Test Setup Screen", `Quick, test_setup_screen);
     ("Test Draw Character", `Quick, test_draw_character);
+    ("Test Draw Line", `Quick, test_draw_line);
   ]
 
 let () =
