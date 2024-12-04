f5688f8bccb1a1ad4d74094c8dd28e60db1c2828
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-12-04 16:12:14 +0000

    feat: colour palette functionality

--- test/test_terminal_screen.ml
+++ test/test_terminal_screen.ml
@@ -17,17 +17,30 @@ let test_draw_character () =
   draw_character [| 10; 5; 0x01; Char.code 'A' |];
 
   let screen_buffer = get_screen_buffer () in
-  assert (screen_buffer.(5).(10) = 'A')
+  let character, colour = screen_buffer.(5).(10) in
+  assert (character = 'A');
+  assert (colour = 0x01)
 
 let test_draw_line () =
-  let test_data = [| 100; 30; 0x01 |] in
+  let test_data = [| 100; 30; 0x02 |] in
   setup_screen test_data;
 
-  draw_line [| 10; 5; 20; 15; 0x01; Char.code '-' |];
+  draw_line [| 10; 5; 20; 15; 42; Char.code '-' |];
 
   let screen_buffer = get_screen_buffer () in
-  assert (screen_buffer.(5).(10) = '-');
-  assert (screen_buffer.(15).(20) = '-')
+  let state = get_screen_state () in
+
+  assert (state.width = 100);
+  assert (state.height = 30);
+
+  let char_at_5_10, colour_at_5_10 = screen_buffer.(5).(10) in
+  let char_at_15_20, colour_at_15_20 = screen_buffer.(15).(20) in
+
+  assert (char_at_5_10 = '-');
+  assert (colour_at_5_10 = 42);
+
+  assert (char_at_15_20 = '-');
+  assert (colour_at_15_20 = 42)
 
 let test_cases =
   [
