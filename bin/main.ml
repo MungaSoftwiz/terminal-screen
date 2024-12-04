ae16109c6c962583660388a9b3b1dbb3df6039d7
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-12-04 16:09:04 +0000

    refactor: integrate colour palette support

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,7 @@ open Terminal_screen.Draw_line
 
 let () =
   (* Setup the screen *)
-  let example_setup_data = [| 80; 25; 0x02 |] in
+  let example_setup_data = [| 80; 25; 0x01 |] in
   try
     setup_screen example_setup_data;
     if is_ready () then
@@ -20,9 +20,9 @@ let () =
     (* Draw characters on the screen *)
     let draw_data_list =
       [
-        [| 10; 15; 0x01; Char.code 'A' |];
-        [| 15; 10; 0x02; Char.code 'B' |];
-        [| 30; 15; 0x03; Char.code 'C' |];
+        [| 10; 15; 44; Char.code 'A' |];
+        [| 15; 10; 46; Char.code 'B' |];
+        [| 30; 15; 12; Char.code 'C' |];
       ]
     in
     List.iter
@@ -34,11 +34,9 @@ let () =
     (* Draw lines on the screen *)
     let line_data_list =
       [
-        [| 5; 5; 20; 5; 0x01; Char.code '-' |];
-        [| 20; 10; 10; 20; 0x02; Char.code '|' |];
-        [| 30; 5; 50; 15; 0x03; Char.code '/' |];
-        [| 50; 15; 40; 20; 0x04; Char.code '*' |];
-        (* [| 0; 24; 79; 24; 0x04; Char.code '*' |]; *)
+        [| 5; 5; 20; 5; 14; Char.code '-' |];
+        [| 20; 10; 10; 20; 22; Char.code '|' |];
+        [| 30; 5; 50; 15; 23; Char.code '/' |];
       ]
     in
     List.iter
@@ -47,13 +45,14 @@ let () =
         with Failure msg -> Printf.printf "Error: %s\n" msg)
       line_data_list;
 
-    (* Display the screen buffer *)
+    (* Display the screen buffer with colours *)
     Printf.printf "Screen buffer:\n";
     let screen_buffer = get_screen_buffer () in
     let state = get_screen_state () in
     for y = 0 to state.height - 1 do
       for x = 0 to state.width - 1 do
-        Printf.printf "%c" screen_buffer.(y).(x)
+        let character, colour_index = screen_buffer.(y).(x) in
+        display_character character colour_index
       done;
       Printf.printf "\n"
     done
