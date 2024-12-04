8db9a2509fdafe482b2a7f1accf7b08807bbec4c
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-12-04 16:03:55 +0000

    docs: documentation for module interface

--- lib/draw_line.mli
+++ lib/draw_line.mli
@@ -1,6 +1,6 @@
 val draw_line : int array -> unit
 (** [draw_line data] draws a line on the terminal screen.
-    [data] is an array of bytes where:
+    @param: data is an array of bytes where:
     - [data.(0)] is the x-coordinate of the starting point.
     - [data.(1)] is the y-coordinate of the starting point.
     - [data.(2)] is the x-coordinate of the ending point.
@@ -8,7 +8,7 @@ val draw_line : int array -> unit
     - [data.(4)] is the colour index (currently unused).
     - [data.(5)] is the ASCII value of the character to draw the line with.
 
-    Raises [Failure] if:
+    @raise Failure if:
     - The screen is not initialized.
     - [data] has an invalid length.
     - The coordinates are out of bounds.
